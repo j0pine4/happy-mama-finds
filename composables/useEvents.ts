56424674dbeebfedb6f4e055ad6f689a869af9f7
Jonah Pine <jpine4@hotmail.com>
2023-07-22 17:03:09 -0400

    only display upcoming events

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -7,7 +7,8 @@ export const useEvents = () => {
     const fetchEvents = () => {
         const {data, error} = useFetch<EventsPaginated>('https://www.eventbriteapi.com/v3/organizations/1627205569163/events/', {
             query: {
-                'page_size': 4
+                'page_size': 4,
+                'status': 'live'
             },
             headers: {
                 'Authorization': 'Bearer ON4PFJ3ERIRHU3YFVUUM'
